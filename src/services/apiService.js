bb9cdd4adfe802b16b97d085bd207f0d489b5b17
frodulec <jakubkrasniewski@gmail.com>
2025-08-21 21:32:46 +0200

    variable change

--- src/services/apiService.js
+++ src/services/apiService.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_BASE_URL = process.env.BACKEND_URL || 'http://localhost:8000';
+const API_BASE_URL = process.env.REACT_APP_BACKEND_URL || 'http://localhost:8000';
 
 const apiClient = axios.create({
   baseURL: API_BASE_URL
